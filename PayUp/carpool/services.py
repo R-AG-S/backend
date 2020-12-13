226db2f8f01375634736aeb21b4f952eb45c42ea
Sandeep <sandeep.pillai42@gmail.com>
2020-12-13 20:19:33 +0530

    heroku

--- PayUp/carpool/services.py
+++
@@ -1,9 +0,0 @@
-from .models import CarPoolRoom
-
-
-def createroom(validated_data):
-    try:
-        room = CarPoolRoom.objects.create(**validated_data)
-        return room.pk
-    except Exception as e:
-        raise e  
--- PayUp/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.urls import path, include
-
-
-urlpatterns = [
-
-
-]
